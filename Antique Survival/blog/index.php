531ba472fd281da4fdb559c8a938cfd22119fb49
Jmakos <dimkoll322@gmail.com>
2020-08-10 16:20:48 +0300

    Made blog page. Text in blog is only for testing purposes at the moment.

--- Antique Survival/blog/index.php
+++ Antique Survival/blog/index.php
@@ -4,60 +4,42 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="resources/css/styles.css">
+        <link rel="stylesheet" type="text/css" href="../resources/css/blog-page-styles.css">
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel&family=Dancing+Script&display=swap" rel="stylesheet"> 
         <meta name="author" content="Jmakos">
         <title>Minecraft | Late night survival server</title>
     </head>
 
     <body>
         <header>
-            <div id="#home"></div>
-            
-            <div>
-                <a href="#home"><img alt="server-logo" src="resources/imgs/Antique Survival_logo.png" width="450px"></a>
+            <div class="light-brown-orange">
+                
+                <div id="server-logo">
+                    <a href="../index.php"><img alt="server-logo" src="../resources/imgs/logo/Antique Survival_logo.png" width="70%"></a>
+                </div>
+
             </div>
         </header>
 
-        <div class="slideshow">
-            <div class="slides fade">
-                <img alt="screenshot1" src="resources/imgs/slideshow/1.png" style="width:100%;">
-            </div>
-
-            <div class="slides fade">
-                <img alt="screenshot2" src="resources/imgs/slideshow/2.png"style="width:100%;">
-            </div>
-
-            <div class="slides fade">
-                <img alt="screenshot3" src="resources/imgs/slideshow/3.png" style="width:100%;">
-            </div>
-            
-            <div class="slides fade">
-                <img alt="screenshot4" src="resources/imgs/slideshow/4.png" style="width:100%;">
-            </div>
-
-            <div class="slides fade">
-                <img alt="screenshot5" src="resources/imgs/slideshow/5.png" style="width:100%;">
-            </div>
+        <div id="title-properties">
+            <h1>Blog - News</h1>
         </div>
 
-        <div>
-            <p>This server is made by MisterFlipFlops and JanoerJJJ.</p>
-
-            <p>
-                <br>Antique Survival is a minecraft server that takes place in a desert city of Antiqua.<br>
-                In the world of Antiqua, each player can build his/her base, fight hordes of aggressive monsters
-                or participate in the tournaments of the local city arena.
-                
-                <br>The world of Antiqua is yours to explore!</br>
-            </p>
+        <div class="flexbox-container light-brown-orange">
+            <div class="blogposts-properties">
+                    <p>
+                        10/08/2020
+                        <br><br><h2 class="post-title-properties">Server grand opening</h2></br></br>
+                        Greetings everyone! We are happy to announce that &quot;<strong>Antique Survival</strong>&quot; 
+                        minecraft server is now open to public. <br>You can enter here: &quot;Public IP&quot;.</br>
+                    </p>
+            </div>
         </div>
 
         <footer>
-            <div>
+            <div class="light-brown-orange" id="footer-properties">
                 All rights reserved &copy; 2020
             </div>
         </footer>
-
-        <script src="resources/js/slideshow.js"></script>
     </body>
 </html>
